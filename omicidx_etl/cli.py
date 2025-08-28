61e15de29a4e6ee4bccb023212943d7a374a120a
Sean Davis <seandavi@gmail.com>
2025-08-28 17:29:18 -0400

    add pubmed command to cli.py

--- omicidx_etl/cli.py
+++ omicidx_etl/cli.py
@@ -8,6 +8,7 @@ from omicidx_etl.ebi_biosample.extract import ebi_biosample
 from omicidx_etl.sra.cli import sra
 from omicidx_etl.geo.extract import geo
 from omicidx_etl.etl.icite import icite
+from omicidx_etl.etl.pubmed import pubmed
 
 
 @click.group()
@@ -23,6 +24,7 @@ cli.add_command(sra)
 cli.add_command(ebi_biosample)
 cli.add_command(geo)
 cli.add_command(icite)
+cli.add_command(pubmed)
 
 if __name__ == "__main__":
     cli()
