55207a57f1c6697ba789461cd18aba950b9c23da
Sean Davis <seandavi@gmail.com>
2025-08-28 16:14:08 -0400

    refactor top-level cli to pull in new commands

--- omicidx_etl/cli.py
+++ omicidx_etl/cli.py
@@ -3,8 +3,10 @@ Main CLI entry point for omicidx-etl.
 """
 
 import click
-from omicidx_etl.biosample.cli import biosample
+from omicidx_etl.biosample.extract import biosample
+from omicidx_etl.ebi_biosample.extract import ebi_biosample
 from omicidx_etl.sra.cli import sra
+from omicidx_etl.geo.extract import geo
 
 
 @click.group()
@@ -17,6 +19,8 @@ def cli():
 # Add subcommands
 cli.add_command(biosample)
 cli.add_command(sra)
+cli.add_command(ebi_biosample)
+cli.add_command(geo)
 
 
 if __name__ == "__main__":
