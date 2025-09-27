0a4be03d98775e3be1a3e685a2d819d950d61b45
Sean Davis <seandavi@gmail.com>
2025-09-27 16:50:12 -0400

    Update europepmc_textmined.py

--- omicidx_etl/etl/europepmc_textmined.py
+++ omicidx_etl/etl/europepmc_textmined.py
@@ -1,10 +1,17 @@
 from upath import UPath
 import polars as pl
-import tqdm
 import click
 from loguru import logger
+import tenacity
 
-OUTPUT_DIR = UPath("/Users/davsean/data/omicidx/europepmc/")
+@tenacity.retry(stop=tenacity.stop_after_attempt(3), wait=tenacity.wait_fixed(2))
+def read_csv_from_remote(csv_file: UPath) -> pl.DataFrame:
+    db = csv_file.stem
+    df = pl.read_csv(str(csv_file), infer_schema_length=1000000).with_columns(
+        db = pl.lit(db)
+    )
+    return df
+
 
 @click.group()
 def europepmc():
@@ -13,28 +20,26 @@ def europepmc():
 
 @europepmc.command("extract")
 @click.argument(
-    'output_dir', 
-    type=click.Path(path_type=UPath), 
+    'output_dir',
+    type=click.Path(path_type=UPath),
 )
 def csv_to_parquet(output_dir: UPath):
     textmined_dir = UPath("https://europepmc.org/pub/databases/pmc/TextMinedTerms/")
-    
-    for i, csv_file in enumerate(list(textmined_dir.glob("*.csv"))):
+
+    csv_file_list = list(textmined_dir.glob("*.csv"))
+
+    for i, csv_file in enumerate(csv_file_list):
         db = csv_file.stem
         logger.info(f"Processing {csv_file}")
-        logger.info(f"File {i+1} of {len(list(textmined_dir.glob('*.csv')))}...")
-        outfile = OUTPUT_DIR / f'{db}_link.parquet'
+        logger.info(f"File {i+1} of {len(csv_file_list)}...")
+        outfile = output_dir / f'{db}_link.parquet'
         outfile.parent.mkdir(parents=True, exist_ok=True)
 
-        df = pl.read_csv(str(csv_file), infer_schema_length=1000000).with_columns(
-            db = pl.lit(db)
-        )
+        df = read_csv_from_remote(csv_file)
         replace_column = df.columns[0]
         df = df.rename({replace_column: "db_id"})
         df.write_parquet(outfile, compression="zstd")
         logger.info(f"Wrote {outfile} with {df.height} rows.")
-    
+
 if __name__ == "__main__":
     csv_to_parquet()
-    logger.info("Europe PMC TextMined terms processed successfully.")
-    logger.info(f"Output files are stored in {OUTPUT_DIR}")
